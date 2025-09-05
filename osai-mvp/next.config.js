5f4c12603428610b66590214c5fe6bc73a53bb1e
MasonIllies <illiesagencies@gmail.com>
2025-09-05 08:48:34 -0400

    Update next.config.js

--- osai-mvp/next.config.js
+++ osai-mvp/next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'osai-seven.vercel.app' }],
+        destination: 'https://www.osai.llc/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 module.exports = nextConfig;
